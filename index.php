eee7a514bb8dc2d7da8c60efd91736cbfc9084ca
Artur Mauricio <arturmauricioss@outlook.com>
2026-01-11 17:53:12 -0300

    adicionando verificador de status do servidor

--- index.php
+++ index.php
@@ -4,22 +4,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP</title>
+    <link rel="shortcut icon" href="https://avallong.com.br/assets/img/logocolorida.png" type="image/x-icon">
 </head>
 <body>
-<h1>Projetos em Python, rodando no servidor do Render</h1>
-<input id="a" type="number" placeholder="A">
-<input id="b" type="number" placeholder="B">
-<button onclick="somar()">Somar</button>
+    <h1>Projetos em Python, rodando no servidor do Render</h1>
+    <p id="server_status">Status: verificando servidor...</p>
+    
+    <input id="a" type="number" placeholder="A">
+    <input id="b" type="number" placeholder="B">
+    <button id="btn_somar" onclick="somar()" disabled>Somar</button>
 
 <p id="resultado"></p>
 
 
   <script>
+    const statusEl = document.getElementById("status");
+    const btn_somar = document.getElementById("btn_somar");
+
+    function atualizarStatus(texto, cor){
+        statusEl.innerText = "Status: " + texto;
+        statusEl.style.color = color;
+    }
+
+    atualizarStatus("tentando ligar...", "orange");
     // acorda o backend quando a página abrir
     fetch("https://avallong-python-backend.onrender.com/")
-      .then(() => console.log("Backend acordado"))
-      .catch(() => console.log("Erro ao acordar backend"));
-
+      .then(res => {
+        if (res.ok){
+        atualizarStatus("Servidor ON", "green");
+        btn_somar.disabled = false;
+      } else {
+        atualizarStatus("Servidor indisponível", "red");
+      }
+    });
+    .catch(() => {
+        atualizarStatus("Servidor OFF", "red");
+    });
+        
     function somar() {
       const a = document.getElementById("a").value;
       const b = document.getElementById("b").value;
@@ -33,6 +54,7 @@
         .catch(err => {
           document.getElementById("resultado").innerText =
             "Erro ao chamar o backend";
+            atualizarStatus("Servidor OFF", "red");
           console.error(err);
         });
     }
